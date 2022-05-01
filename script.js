2b00ff3ce96c59418c84b02e33fe07e1de21c5c7
Alex de Roos <100049383+DinoSkeleton100@users.noreply.github.com>
2022-05-01 16:39:04 +0200

    yas queen

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 $(document).ready(function() {
 
+    const englishTranslate = [
+        "English",
+        "Dutch",
+        "Gender:",
+        "man",
+        "woman",
+        "Name:"
+    ];
+
+    const dutchTranslate = [
+        "Engels",
+        "Nederlands",
+        "Geslacht:",
+        "man",
+        "vrouw",
+        "Naam:"
+    ];
+
     //language knoppen en vertalen
     $('#englishButton').on('click', function() {
-
+        for (var i = 0; i < englishTranslate.length; i++) {
+            $('[data-languageTranslateId='+i+']').text(englishTranslate[i]);
+        }
     });
 
     $('#dutchButton').on('click', function() {
-
+        for (var i = 0; i < dutchTranslate.length; i++) {
+            $('[data-languageTranslateId='+i+']').text(dutchTranslate[i]);
+        }
     });
 
     //gender en kleurverandering >>
