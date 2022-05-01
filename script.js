864c19bc5572be02ad7c5f84de69c980d1e8db91
Alex de Roos <100049383+DinoSkeleton100@users.noreply.github.com>
2022-05-01 15:33:11 +0200

    little startup

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
-    
-    $('genderForm').change(function() {
-        alert(changes);
-        if ($('genderForm').val() == woman) {
-            $('.man').addClass('woman');
-            $('.man').removeClass('man');
-        }
+
+    //language knoppen en vertalen
+    $('#englishButton').on('click', function() {
+
     });
 
+    $('#dutchButton').on('click', function() {
+
+    });
+
+    //gender en kleurverandering >>
+    $('#womanButton').on('click', function() {
+        $('.man').addClass('woman');
+        $('.man').removeClass('man');
+        $('body').css('background-color','rgb(118, 68, 138)');
+    });
+
+    $('#manButton').on('click', function() {
+        $('.woman').addClass('man');
+        $('.woman').removeClass('woman');
+        $('body').css('background-color','rgb(14, 102, 85)');
+    });
+    //<<
 });
