869aa9cd1f24446c7d732ee02e5a135be13059cc
Alex de Roos <100049383+DinoSkeleton100@users.noreply.github.com>
2022-05-01 14:14:31 +0200

    it doesnt work yet

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     
+    $('genderForm').change(function() {
+        alert(changes);
+        if ($('genderForm').val() == woman) {
+            $('.man').addClass('woman');
+            $('.man').removeClass('man');
+        }
+    });
+
 });
